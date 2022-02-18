7078b8017df897348099109ff64e2fd7e439fa37
PickleEric <60155510+PickleEric@users.noreply.github.com>
2022-02-17 22:26:52 -0600

    added reviews and skills array to data.js

--- src/data.js
+++ src/data.js
@@ -5,7 +5,7 @@ export const projects = [
       description:
         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
       image: "./project-1.gif",
-      link: "https://reactbootcamp.com",
+      link: "https://reactbootcamp.com"
     },
     {
       title: "React Tracks",
@@ -13,11 +13,20 @@ export const projects = [
       description:
         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
       image: "./project-2.gif",
-      link: "https://reedbarger.com",
+      link: "https://reedbarger.com"
     }
   ];
   export const skills = [
     {
       skills: "iasda asiibo aewoaskdfljkljsddlkasdjf, l;asdjkf;asdifjori, oasidjfo aosidfj"
+    }
+  ]
+
+  export const reviews = [
+    {
+      quote: '"sdfasdfkjl dkl ifj lajfi oiejfgopspo!"',
+      image: "./DonPond.jpg",
+      name: "Resturant",
+      company: "Catering"
     }
   ]
